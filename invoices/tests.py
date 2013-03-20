e17ef32cdda556ed3adb88e10b51ce8cd98df295
Niels Sandholt Busch <niels.busch@gmail.com>
2013-03-20 18:22:12 +0100

    update tests and readme

--- invoices/tests.py
+++ invoices/tests.py
@@ -5,19 +5,26 @@ from datetime import date, datetime
 from django import test
 
 from invoices import create_invoice
-from invoices.models import Invoice, Item, LineItem, LineItemType, LineItemGroup
 
 class InvoiceTestCase(test.TestCase):	
 
     def test_create_invoice(self):
-        invoice = create_invoice(date.today(), date.today(), currency='EUR', country='de', items=[
-            {
+        """
+        Test creating an invoice
+
+        """
+        invoice = create_invoice(
+            date.today(), date.today(), currency='EUR', country='de', items=[{
                 'name': 'The fish',
-                'lineItemGroups': [('standard', 'hejsa', [('line item description', Decimal(1), date.today(),)])],
-            },
-            {
+                'lineItemGroups': [('standard', 'hejsa', [
+                    ('line item description', Decimal(1), date.today())
+                ])],
+            }, {
                 'name': 'Consulting',
-                'lineItemGroups': [('weekend', 'davs', [('7 hours tough work', Decimal(1), date.today(),)])],
+                'lineItemGroups': [('weekend', 'davs', [
+                    ('7 hours tough work', Decimal(1), date.today())
+                ])]
             },
         ])
         self.assertEquals(invoice.total_amount, Decimal("2.38"))
+        self.assertEquals(invoice.is_paid, False)
