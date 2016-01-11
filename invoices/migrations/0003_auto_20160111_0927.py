085273439c30a03f57257e4b6e8d4906cab89223
Niels Sandholt Busch <niels.busch@gmail.com>
2016-01-11 16:29:55 +0100

    add payment reminder

---
+++ invoices/migrations/0003_auto_20160111_0927.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('invoices', '0002_invoice_owner'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='invoice',
+            name='status',
+            field=models.PositiveIntegerField(default=0, db_index=True),
+        ),
+        migrations.AddField(
+            model_name='invoice',
+            name='status_updated',
+            field=models.DateTimeField(default=datetime.datetime(2016, 1, 11, 15, 27, 13, 898828, tzinfo=utc), auto_now_add=True),
+            preserve_default=False,
+        ),
+        migrations.AlterField(
+            model_name='invoice',
+            name='owner',
+            field=models.ForeignKey(related_name='invoices', blank=True, to=settings.AUTH_USER_MODEL, null=True),
+        ),
+        migrations.AlterField(
+            model_name='invoice',
+            name='user',
+            field=models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True),
+        ),
+    ]
--- invoices/models.py
+++ invoices/models.py
@@ -13,6 +13,11 @@ from invoices.signals import invoice_ready, invoice_confirmed
 
 RELATED_MODEL = getattr(settings, 'INVOICES_RELATED_MODEL', User)
 
+STATUS_UNCONFIRMED = 0
+STATUS_CONFIRMED = 1
+STATUS_PAYMENT_REMINDER = 2
+STATUS_DUNNING_1 = 3
+STATUS_DUNNING_2 = 4
 
 class InvoicesBaseModel(models.Model):
     class Meta:
@@ -106,6 +111,9 @@ class Invoice(InvoicesBaseModel):
                                    null=True,
                                    related_name='cancelled_by')
     confirmed = models.BooleanField(default=True)
+    status = models.PositiveIntegerField(default=STATUS_UNCONFIRMED,
+                                         db_index=True)
+    status_updated = models.DateTimeField(auto_now_add=True)
     sequence_number = models.PositiveIntegerField(null=True)
 
     @property
