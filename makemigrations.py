d812f73d6eba7d72be41bde2b359d718c6f3e3ef
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2016-11-16 15:23:06 +0100

    makemigrations.py script

---
+++ makemigrations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+import django
+
+from django.conf import settings
+from django.core.management import call_command
+
+settings.configure(
+    DEBUG=True,
+    INSTALLED_APPS=(
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'invoices',
+    ),
+)
+
+django.setup()
+call_command('makemigrations', 'invoices')
